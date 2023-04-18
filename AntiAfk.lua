eaf1ca8f5736f1dffb542daa3be4d0e12cf100ac
VexTheVex <131091039+VexTheVex@users.noreply.github.com>
2023-04-17 21:11:59 -0500

    Update AntiAfk.lua

--- AntiAfk.lua
+++ AntiAfk.lua
@@ -1,19 +1,32 @@
-wait(0.5)local ba=Instance.new("ScreenGui")
-local ca=Instance.new("TextLabel")local da=Instance.new("Frame")
-local _b=Instance.new("TextLabel")local ab=Instance.new("TextLabel")ba.Parent=game.CoreGui
-ba.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;ca.Parent=ba;ca.Active=true
-ca.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ca.Draggable=true
-ca.Position=UDim2.new(0.698610067,0,0.098096624,0)ca.Size=UDim2.new(0,370,0,52)
-ca.Font=Enum.Font.SourceSansSemibold;ca.Text="Anti AFK Script"ca.TextColor3=Color3.new(0,1,1)
-ca.TextSize=22;da.Parent=ca
-da.BackgroundColor3=Color3.new(0.196078,0.196078,0.196078)da.Position=UDim2.new(0,0,1.0192306,0)
-da.Size=UDim2.new(0,370,0,107)_b.Parent=da
-_b.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)_b.Position=UDim2.new(0,0,0.800455689,0)
-_b.Size=UDim2.new(0,370,0,21)_b.Font=Enum.Font.Arial;_b.Text=" VexTheVex "
-_b.TextColor3=Color3.new(0,1,1)_b.TextSize=20;ab.Parent=da
-ab.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ab.Position=UDim2.new(0,0,0.158377,0)
-ab.Size=UDim2.new(0,370,0,44)ab.Font=Enum.Font.ArialBold;ab.Text="Status: Active"
-ab.TextColor3=Color3.new(0,1,1)ab.TextSize=20;local bb=game:service'VirtualUser'
-game:service'Players'.LocalPlayer.Idled:connect(function()
-bb:CaptureController()bb:ClickButton2(Vector2.new())
-ab.Text="Roblox tried to kick u but i kicked him instead"wait(2)ab.Text="Status : Active"end)
+-- Anti AFK script suitable for putting in autoexec.
+-- This script create by MaxproGlitcher#6199
+
+while not game:IsLoaded() do wait() end
+
+repeat wait() until game.Players.LocalPlayer.Character
+
+Players = game:GetService("Players")
+
+local GC = getconnections or get_signal_cons
+if GC then
+	for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
+		if v["Disable"] then v["Disable"](v)
+		elseif v["Disconnect"] then v["Disconnect"](v)
+		end
+	end
+else
+  Players.LocalPlayer.Idled:Connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+  end)
+end
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+  Title = " ANTI-AFK EXERCUTER", 
+  Text = "YIPEEEEE", 
+  Icon = "rbxassetid://11823384169",
+  Duration = 15
+})
+
+game.StarterGui:SetCore('SendNotification', { Title = 'Anti-AFK'; Text = 'Script create by VexTheVex Anti-AFK activated'; })
+game.StarterGui:SetCore('SendNotification', { Title = 'Anti-AFK'; Text = 'Version 1.0'; })
